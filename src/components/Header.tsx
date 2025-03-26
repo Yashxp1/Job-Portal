2b36dbdb488df2330faa46a653668fb851b76eea
Yash Raj <kyashraj991@gmail.com>
2025-03-27 00:23:50 +0530

    Clerk Auth setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
 import { Link } from 'react-router-dom';
 import { Button } from './ui/button';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from '@clerk/clerk-react';
 
 const Header = () => {
   return (
@@ -8,10 +14,18 @@ const Header = () => {
         <Link to="/">
           <img src="/logo.png" className="h-20" />
         </Link>
-        <Button variant="outline">Login</Button>
+
+        {/* <Button variant="outline">Login</Button> */}
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </nav>
     </>
   );
 };
 
 export default Header;
+
