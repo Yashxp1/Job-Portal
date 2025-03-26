1e7bdb0b51990241ae673ce49048a99fc1e90987
Yash Raj <kyashraj991@gmail.com>
2025-03-27 00:41:23 +0530

    Supabase and clerk config

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,9 +1,20 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-import { createClient } from '@supabase/supabase-js';
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
-export default supabase
-        
+const supaBaseClient = async (
+  supabaseAcessToken: string
+): Promise<SupabaseClient> => {
+  return createClient(supabaseUrl, supabaseKey, {
+    global: {
+      headers: {
+        Authorization: `Bearer ${supabaseAcessToken}`,
+      },
+    },
+  });
+};
+
+export default supaBaseClient;
